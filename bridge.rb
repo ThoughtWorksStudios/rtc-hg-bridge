2ede648cf4fd82924bd49618c1b05df760d1e466
benbc <ben@bridesmere.com>
2011-06-30 17:25:18 +0100

    Hypothetica RTC version.

--- bridge.rb
+++ bridge.rb
@@ -2,7 +2,7 @@ WORKING_COPY = '/tmp/bridge'
 HG_REPO = 'http://localhost:8000'
 
 class Bridge
-  def initialize() @scm = SCM.new end
+  def initialize() @scm = GitSCM.new end
 
   def setup
     File.exists? WORKING_COPY and rm_r WORKING_COPY
@@ -17,12 +17,14 @@ class Bridge
     cd WORKING_COPY do
       @scm.update_working_copy
       logs = @scm.get_logs
-      if File.exists? "#{WORKING_COPY}/.last"
-        last = IO.read("#{WORKING_COPY}/.last").strip
-        logs = logs.take_while { |log| log.revision != last }
-      end
-      logs.reverse!
-      last = nil
+      # With RTC we can ensure that we only get the changes we need.
+      #
+      # if File.exists? "#{WORKING_COPY}/.last"
+      #   last = IO.read("#{WORKING_COPY}/.last").strip
+      #   logs = logs.take_while { |log| log.revision != last }
+      # end
+      # logs.reverse!
+      # last = nil
       logs.each do |log|
         last = log.revision
         @scm.change_working_copy_to_revision(log.revision)
@@ -31,12 +33,12 @@ class Bridge
         sh "hg push --quiet #{HG_REPO}"
       end
       @scm.clean_up_working_copy
-      last and File.open("#{WORKING_COPY}/.last", 'w') { |f| f.write(last) }
+      # last and File.open("#{WORKING_COPY}/.last", 'w') { |f| f.write(last) }
     end
   end
 end
 
-class SCM
+class GitSCM
   def make_working_copy
     sh "git clone /tmp/src ."
   end
@@ -67,6 +69,40 @@ class SCM
 
     def message
       @line.split[1..-1].join(' ')
+    end
+  end
+end
+
+class RTCSCM
+  def make_working_copy
+    sh "scm create workspace --username ben --password ben --repository-uri https://localhost:9443/ccm -s 'BRM Stream' ben-stream"
+    sh "scm load --username ben --password ben ben-stream@https://localhost:9443/ccm"
+  end
+
+  def update_working_copy
+    # not needed for RTC
+  end
+
+  def get_logs
+    `scm compare ws "BRM Stream" stream ben-stream --password ben --include-types s`.
+      lines.map { |line| Log.new(line) }
+  end
+
+  def change_working_copy_to_revision(revision)
+    sh "scm accept --password ben --changes #{revision}"
+  end
+
+  def clean_up_working_copy
+    # not needed for RTC
+  end
+
+  class Log
+    attr_reader :revision, :message
+
+    def initialize(line)
+      parse = /\(([0-9])\) (.*)/.match(line)
+      @revision = parse[1]
+      @message = parse[2]
     end
   end
 end
