401b690edcbbbc13c25f741b5bed77cad7ddb1f6
benbc <ben@bridesmere.com>
2011-06-30 17:26:52 +0100

    Remove Git stuff because it's only making things more complicated.

--- bridge.rb
+++ bridge.rb
@@ -2,7 +2,7 @@ WORKING_COPY = '/tmp/bridge'
 HG_REPO = 'http://localhost:8000'
 
 class Bridge
-  def initialize() @scm = GitSCM.new end
+  def initialize() @scm = SCM.new end
 
   def setup
     File.exists? WORKING_COPY and rm_r WORKING_COPY
@@ -15,16 +15,7 @@ class Bridge
 
   def run
     cd WORKING_COPY do
-      @scm.update_working_copy
       logs = @scm.get_logs
-      # With RTC we can ensure that we only get the changes we need.
-      #
-      # if File.exists? "#{WORKING_COPY}/.last"
-      #   last = IO.read("#{WORKING_COPY}/.last").strip
-      #   logs = logs.take_while { |log| log.revision != last }
-      # end
-      # logs.reverse!
-      # last = nil
       logs.each do |log|
         last = log.revision
         @scm.change_working_copy_to_revision(log.revision)
@@ -32,55 +23,14 @@ class Bridge
         sh "hg commit -m '#{log.revision}: #{log.message}' -ubridge"
         sh "hg push --quiet #{HG_REPO}"
       end
-      @scm.clean_up_working_copy
-      # last and File.open("#{WORKING_COPY}/.last", 'w') { |f| f.write(last) }
     end
   end
 end
 
-class GitSCM
-  def make_working_copy
-    sh "git clone /tmp/src ."
-  end
-
-  def update_working_copy
-    sh "git pull --quiet"
-  end
-
-  def get_logs
-    `git log --oneline`.lines.map { |line| Log.new(line) }
-  end
-
-  def change_working_copy_to_revision(revision)
-    sh "git checkout --quiet #{revision}"
-  end
-
-  # may only be necessary for git
-  def clean_up_working_copy
-    sh "git checkout --quiet master"
-  end
-
-  class Log
-    def initialize(line) @line = line end
-
-    def revision
-      @line.split.first
-    end
-
-    def message
-      @line.split[1..-1].join(' ')
-    end
-  end
-end
-
-class RTCSCM
+class SCM
   def make_working_copy
     sh "scm create workspace --username ben --password ben --repository-uri https://localhost:9443/ccm -s 'BRM Stream' ben-stream"
     sh "scm load --username ben --password ben ben-stream@https://localhost:9443/ccm"
-  end
-
-  def update_working_copy
-    # not needed for RTC
   end
 
   def get_logs
@@ -90,10 +40,6 @@ class RTCSCM
 
   def change_working_copy_to_revision(revision)
     sh "scm accept --password ben --changes #{revision}"
-  end
-
-  def clean_up_working_copy
-    # not needed for RTC
   end
 
   class Log
